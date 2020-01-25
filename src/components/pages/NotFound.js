27b279701d755e385fac4d6c08a41ed1d48101df
Guilherme Conti <guibasconti@gmail.com>
2020-01-25 04:00:20 -0300

    Improve not found page

--- src/components/pages/NotFound.js
+++ src/components/pages/NotFound.js
@@ -1,14 +1,14 @@
 import React from 'react';
 import { Link } from 'react-router-dom';
+import PageWrapper from '../elements/shared/PageWrapper';
+import Header from '../elements/shared/Header';
 
 const NotFoundPage = () => {
   return (
-    <div>
-      <h4>
-        404 Page Not Found
-      </h4>
+    <PageWrapper>
+      <Header title="Page not found" />
       <Link to="/"> Go back to homepage </Link>
-    </div>
+    </PageWrapper>
   );
 };
 
