7354c850f5214d37c4bdf6d8cf900faa209c6470
Guilherme Conti <guibasconti@gmail.com>
2020-01-25 21:21:31 -0300

    Add tests to store

--- src/store/store.spec.js
+++ src/store/store.spec.js
@@ -1,48 +1,51 @@
 import configureStore from './index';
+import { CHANGE_DIFFICULTY } from '../constants/creationTypes';
+import { NEW_GAME, GAME_INFO } from '../constants/gameTypes';
 
 describe('Store', () => {
   beforeAll(() => {});
   afterAll(() => {});
 
-  it('should display results when necessary foo data is provided', () => {
+  it('should display results when necessary creation data is provided', () => {
     const store = configureStore();
 
-    // const actions = [
-    //   { type: FOO_EXAMPLE, payload: { value: 20 } },
-    //   {
-    //     type: FOO_ANOTHER_EXAMPLE,
-    //     payload: { fieldName: 'anotherExample', value: 10 }
-    //   }
-    // ];
-    // actions.forEach(action => store.dispatch(action));
+    const actions = [
+      { type: CHANGE_DIFFICULTY, payload: { difficulty: '0' } },
+    ];
+    actions.forEach(action => store.dispatch(action));
 
-    // const actual = store.getState();
-    // const expected = {
-    //   anotherExample: 10,
-    //   example: 20
-    // };
+    const actual = store.getState();
+    const expected = {
+      difficulty: '0',
+    };
 
-    // expect(actual.foo).toEqual(expected);
+    expect(actual.creation).toEqual(expected);
   });
 
-  it('should display results when necessary bar data is provided', () => {
+  it('should display results when necessary game data is provided', () => {
     const store = configureStore();
+    const game = {
+      id: 'id',
+      board: ['*'],
+      status: 0,
+      difficulty: '0',
+      rows: 10,
+      columns: 10,
+      mines: 10,
+      seed: '',
+      loading: false,
+      found: true,
+    };
 
-    // const actions = [
-    //   { type: BAR_EXAMPLE, payload: { value: 1.5 } },
-    //   {
-    //     type: BAR_ANOTHER_EXAMPLE,
-    //     payload: { fieldName: 'anotherExample', value: 1.5 }
-    //   }
-    // ];
-    // actions.forEach(action => store.dispatch(action));
+    const actions = [
+      { type: NEW_GAME, payload: game },
+      { type: GAME_INFO, payload: game },
+    ];
+    actions.forEach(action => store.dispatch(action));
 
-    // const actual = store.getState();
-    // const expected = {
-    //   example: 1.5,
-    //   anotherExample: 1.5
-    // };
+    const actual = store.getState();
+    const expected = game;
 
-    // expect(actual.bar).toEqual(expected);
+    expect(actual.game).toEqual(expected);
   });
 });
